38f9ad3ff503a21ced6320604e2a195c615d1299
kousuke takahashi <pamarikomi_t@icloud.com>
2021-08-26 22:54:22 +0900

    create connection pool

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -11,11 +11,17 @@ const timeout = 1 * time.Second
 
 func main() {
 	c := pkg.NewClient(":5000", timeout)
+	defer c.Close()
 
 	resp, err := c.Ping([]byte("hello"))
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(resp)
 
+	resp, err = c.Ping([]byte("hello2"))
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println(resp)
 }
--- pkg/client.go
+++ pkg/client.go
@@ -2,12 +2,16 @@ package pkg
 
 import (
 	"net"
+	"sync"
 	"time"
 )
 
 type Client struct {
 	Addr    string
 	Timeout time.Duration
+
+	mu   sync.Mutex
+	conn net.Conn // 構造体にconnをもたせてコネクションプール
 }
 
 func NewClient(addr string, timeout time.Duration) *Client {
@@ -18,32 +22,66 @@ func NewClient(addr string, timeout time.Duration) *Client {
 }
 
 func (c *Client) Ping(b []byte) (string, error) {
-	conn, err := net.DialTimeout("tcp", c.Addr, c.Timeout)
-	if err != nil {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if err := c.connect(); err != nil {
 		return "", err
 	}
-	defer conn.Close()
 
 	if c.Timeout > 0 {
-		if err = conn.SetDeadline(time.Now().Add(c.Timeout)); err != nil {
+		if err := c.conn.SetDeadline(time.Now().Add(c.Timeout)); err != nil {
 			return "", err
 		}
 	}
-	_, err = conn.Write(b)
+	_, err := c.conn.Write(b)
 	if err != nil {
 		return "", err
 	}
 
 	buf := make([]byte, 1024)
 	if c.Timeout > 0 {
-		if err = conn.SetDeadline(time.Now().Add(c.Timeout)); err != nil {
+		if err = c.conn.SetDeadline(time.Now().Add(c.Timeout)); err != nil {
 			return "", err
 		}
 	}
-	n, err := conn.Read(buf)
+	n, err := c.conn.Read(buf)
 	if err != nil {
 		return "", err
 	}
 
 	return string(buf[:n]), nil
+}
+
+func (c *Client) Close() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.close()
+}
+
+func (c *Client) Connect() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.connect()
+}
+
+func (c *Client) close() error {
+	if c.conn != nil {
+		err := c.conn.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *Client) connect() error {
+	if c.conn == nil {
+		conn, err := net.DialTimeout("tcp", c.Addr, c.Timeout)
+		if err != nil {
+			return err
+		}
+		c.conn = conn
+	}
+	return nil
 }
